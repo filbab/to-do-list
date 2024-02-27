a37df0ee96d1e71a7049737abe540b06e69135b9
filbab <filbab@wp.pl>
2024-02-27 21:05:26 +0100

    routes (in progress)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Create task
+
+// List tasks
+Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+// Update task
+
+// Delete task
+
+// Assign user
+
+// Change status
