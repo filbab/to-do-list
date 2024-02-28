cec53458c9e4dfc15b8530da616da4dc5a6d1ca8
filbab <filbab@wp.pl>
2024-02-28 21:40:48 +0100

    create TaskQuery class

---
+++ domain/Task/Queries/TaskQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Task\Queries;
+
+use Illuminate\Support\Collection;
+use Domain\Task\Entities\Models\Task;
+use App\Http\Requests\Task\ShowTasksRequest;
+
+class TaskQuery
+{
+   private Task $task;
+
+   public function __construct()
+   {
+      $this->task = new Task();
+   }
+
+   public function getTasks(ShowTasksRequest $request): ?Collection
+   {
+      $query = $this->task::query();
+      
+      if ($request->getTitle()) {
+         $query->byTitle($request->getTitle());
+      }
+
+      if ($request->getStatus()) {
+         $query->byStatus($request->getStatus());
+      }
+
+      if ($request->getUserIds()) {
+         $query->byUserIds($request->getUserIds());
+      }
+
+      return $query->get();
+   }
+}
