e1ea50b58bb112fe060025b0d3b785f54f9f2a26
filbab <filbab@wp.pl>
2024-02-28 21:39:42 +0100

    update tasks table - add composite index

--- database/migrations/2024_02_27_093224_create_tasks_table.php
+++ database/migrations/2024_02_27_093224_create_tasks_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            
             $table->string('title');
             $table->text('description');
             $table->enum('status', ['pending', 'in progress', 'completed']);
+
+            $table->index(['status', 'title']);
+
             $table->timestamps();
         });
     }
