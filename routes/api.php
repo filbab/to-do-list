0d4ebab07f8e7f43319ce0a114d2b21fb1eed637
filbab <filbab@wp.pl>
2024-03-02 20:33:03 +0100

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Responses\TaskDoesNotExist;
 
@@ -16,26 +17,29 @@ use App\Http\Responses\TaskDoesNotExist;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+   // List tasks
+   Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+   // Create task (with status and users)
+   Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+   // Update task (with status and users)
+   Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update')
+      ->missing(function () {
+         return new TaskDoesNotExist();
+      });
+
+   // Delete task
+   Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy')
+      ->missing(function () {
+         return new TaskDoesNotExist();
+      });
 });
-// List tasks
-Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-// Create task (with status and users)
-Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
-
-// Update task (with status and users)
-Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update')
-   ->missing(function () {
-      return new TaskDoesNotExist();
-   });
-
-// Delete task
-Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy')
-   ->missing(function () {
-      return new TaskDoesNotExist();
-   });
